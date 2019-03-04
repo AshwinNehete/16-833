50ff79de2334c812b690ec57ffdf8692cf400c7f
Abhay Gupta <gupta94abhay@gmail.com>
2019-03-04 12:13:37 -0500

    HW2 initial commit

---
+++ hw2/EKF_slam/EKF_SLAM.m
@@ -0,0 +1,99 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  16833 Robot Localization and Mapping  % 
+%  Assignment #2                         %
+%  EKF-SLAM                              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear;
+close all;
+clc;
+
+%==== TEST: Setup uncertainity parameters (try different values!) ===
+sig_x = 0.25;
+sig_y = 0.1;
+sig_alpha = 0.1;
+sig_beta = 0.01;
+sig_r = 0.08;
+
+%==== Generate sigma^2 from sigma ===
+sig_x2 = sig_x^2;
+sig_y2 = sig_y^2;
+sig_alpha2 = sig_alpha^2;
+sig_beta2 = sig_beta^2;
+sig_r2 = sig_r^2;
+
+%==== Open data file ====
+fid = fopen('../data/data.txt');
+
+%==== Read first measurement data ====
+tline = fgets(fid);
+arr = str2num(tline);
+measure = arr';
+t = 1;
+ 
+%==== Setup control and measurement covariances ===
+control_cov = diag([sig_x2, sig_y2, sig_alpha2]);
+measure_cov = diag([sig_beta2, sig_r2]);
+
+%==== Setup initial pose vector and pose uncertainty ====
+pose = [0 ; 0 ; 0];
+pose_cov = diag([0.02^2, 0.02^2, 0.1^2]);
+
+%==== TODO: Setup initial landmark vector landmark[] and covariance matrix landmark_cov[] ====
+%==== (Hint: use initial pose with uncertainty and first measurement) ====
+
+% Write your code here...
+
+
+%==== Setup state vector x with pose and landmark vector ====
+x = [pose ; landmark];
+
+%==== Setup covariance matrix P with pose and landmark covariances ====
+P = [pose_cov zeros(3, 2*k) ; zeros(2*k, 3) landmark_cov];
+
+%==== Plot initial state and conariance ====
+last_x = x;
+drawTrajAndMap(x, last_x, P, 0);
+
+%==== Read control data ====
+tline = fgets(fid);
+while ischar(tline)
+    arr = str2num(tline);
+    d = arr(1);
+    alpha = arr(2);
+    
+    %==== TODO: Predict Step ====
+    %==== (Notice: predict state x_pre[] and covariance P_pre[] using input control data and control_cov[]) ====
+    
+    % Write your code here...
+    
+    
+    %==== Draw predicted state x_pre[] and covariance P_pre[] ====
+    drawTrajPre(x_pre, P_pre);
+    
+    %==== Read measurement data ====
+    tline = fgets(fid);
+    arr = str2num(tline);
+    measure = arr';
+    
+    %==== TODO: Update Step ====
+    %==== (Notice: update state x[] and covariance P[] using input measurement data and measure_cov[]) ====
+    
+    % Write your code here...
+    
+    
+    %==== Plot ====   
+    drawTrajAndMap(x, last_x, P, t);
+    last_x = x;
+    
+    %==== Iteration & read next control data ===
+    t = t + 1;
+    tline = fgets(fid);
+end
+
+%==== EVAL: Plot ground truth landmarks ====
+
+% Write your code here...
+    
+
+%==== Close data file ====
+fclose(fid);
