0861baac9b3739b2c9eeb87ee5b19e484e897300
Abhay Gupta <gupta94abhay@gmail.com>
2019-03-18 20:02:14 -0400

    Adding hw3 init

---
+++ hw3/slam_solvers/linear/solve_qr2.m
@@ -0,0 +1,20 @@
+% SOLVE_QR2
+% 16-833 Spring 2019 - *Stub* Provided
+% Solves linear system using second QR method
+%
+% Arguments: 
+%     A     - A matrix from linear system that you generate yourself
+%     b     - b vector from linear system that you generate yourself
+%
+% Returns:
+%     x     - solution to the linear system, computed using the specified
+%             version of the QR decomposition
+%     R     - R factor from the QR decomposition
+%
+function [x, R] = solve_qr2(A, b)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%% Your code goes here %%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+end
